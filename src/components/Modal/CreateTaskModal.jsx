3207c7bfaf4add88381d9098a8efa9298dce414c
Daniel Marcovich <danielmark999@gmail.com>
2023-03-20 03:43:57 -0500

    refactor(components): refactoring code components
    refactor: CreateTaskModal, handleSubnmit| TaskContext: general name of functions and vairable,
    initialStateTask | | taskReducer: update ADD_TASK case  | uninstall: crypto-js

--- src/components/Modal/CreateTaskModal.jsx
+++ src/components/Modal/CreateTaskModal.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { TaskContext } from '../../context/TaskContext';
+import { TYPES } from '../../actions/taskActions'
+import { ID, CALC_DAYS } from '../../constants/constants';
 import css from './Modal.module.scss'
 
 export const CreateTaskModal = () => {
     const [form, setForm] = useState({});
 
-    const onHandleSubmit = e => {
+    const { dispatch } = useContext(TaskContext);
+
+    const handleSubmit = e => {
         e.preventDefault();
-        console.log(form);
+        const newId = ID.newId();
+        const days = CALC_DAYS(form.start, form.end)
+        dispatch({ type: TYPES.ADD_TASK, payload: { ...form, id: newId, days } })
     }
 
     const handleChange = e => {
@@ -17,62 +24,74 @@ export const CreateTaskModal = () => {
         setForm({ ...form, [e.target.name]: e.target.value });
     }
 
+    const resetForm = () => setForm({});
+
     return (
         <>
             <form
                 className={css["c-form"]}
-                onSubmit={onHandleSubmit}
+                onSubmit={handleSubmit}
             >
                 {/* ISSUE */}
-                <label
-                    htmlFor="issue"
-                    className={css.label}>Issue:</label>
-                <input
-                    id="create-issue-input"
-                    type="text"
-                    name="issue"
-                    className={css.input}
-                    onChange={handleChange}
-                />
+                <div className={css["c-input"]}>
+                    <label
+                        htmlFor="issue"
+                        className={css.label}>Issue:</label>
+                    <input
+                        id="create-issue-input"
+                        type="text"
+                        name="issue"
+                        className={css.input}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
 
                 {/* DESCRIPTION */}
-                <label
-                    htmlFor="description"
-                    className={css.label}>Description:</label>
-                <input
-                    id="create-description-input"
-                    type="text"
-                    name="description"
-                    className={css.input}
-                    onChange={handleChange}
-                />
+                <div className={css["c-input"]}>
+                    <label
+                        htmlFor="description"
+                        className={css.label}>Description:</label>
+                    <input
+                        id="create-description-input"
+                        type="text"
+                        name="description"
+                        className={css.input}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
 
-                {/* START DATE */}
-                <label
-                    htmlFor="start"
-                    className={css.label}>Start date</label>
-                <input
-                    id="create-start-data"
-                    type="date"
-                    name="start"
-                    className={css.input}
-                    onChange={handleDate}
-                />
+                <div className={css["c-input"]}>
+                    {/* START DATE */}
+                    <label
+                        htmlFor="start"
+                        className={css.label}>Start date</label>
+                    <input
+                        id="create-start-data"
+                        type="date"
+                        name="start"
+                        className={css.input}
+                        onChange={handleDate}
+                        required
+                    />
 
-                {/* END DATE */}
-                <label
-                    htmlFor="end"
-                    className={css.label}>End date</label>
-                <input
-                    id="create-end-data"
-                    type="date"
-                    name="end"
-                    className={css.input}
-                    onChange={handleDate}
-                />
+                    {/* END DATE */}
+                    <label
+                        htmlFor="end"
+                        className={css.label}>End date</label>
+                    <input
+                        id="create-end-data"
+                        type="date"
+                        name="end"
+                        className={css.input}
+                        onChange={handleDate}
+                    />
+                </div>
 
                 {/* SUBMIT BTN */}
-                <button type="submit">Create</button>
+                <button type="submit" value="create issue">Create</button>
+                <button type="reset" onClick={resetForm}>Clear</button>
             </form>
         </>
     )
--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -5,6 +5,7 @@ import {
 	faBars,
 	faXmark
 } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from 'uuid';
 
 
 export const DAYS = {
@@ -20,3 +21,19 @@ export const ICONS = {
 	drag: <FontAwesomeIcon id="icon-drag" icon={faBars} />,
 	close: <FontAwesomeIcon id="icon-close" fixedWidth icon={faXmark} />
 };
+
+export const ID = {
+	newId: () => uuidv4()
+};
+
+export const CALC_DAYS = (startDate, endDate) => {
+	const msPerDay = 1000 * 60 * 60 * 24;
+
+	const start = new Date(startDate);
+	const end = new Date(endDate);
+
+	const msDif = end - start;
+	const daysDif = Math.round(msDif / msPerDay) + 1;
+
+	return daysDif;
+}
